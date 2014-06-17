4788bd04e6e31816b2fe69db38200f6d4fae24bd
taabi <taabi@taabi-macbookpro.roam.corp.google.com>
2014-06-16 18:45:25 -0700

    Initial Commit

---
+++ Taabi-tipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Taabi-tipCalculator
+//
+//  Created by taabi on 6/16/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Taabi-tipCalculatorTests/Taabi_tipCalculatorTests.m
@@ -0,0 +1,34 @@
+//
+//  Taabi_tipCalculatorTests.m
+//  Taabi-tipCalculatorTests
+//
+//  Created by taabi on 6/16/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Taabi_tipCalculatorTests : XCTestCase
+
+@end
+
+@implementation Taabi_tipCalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
